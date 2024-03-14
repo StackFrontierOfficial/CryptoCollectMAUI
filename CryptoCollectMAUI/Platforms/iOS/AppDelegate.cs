455067d08dfce17661740888221535e181e17c20
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:43:57 +0530

    Initial Push

---
+++ CryptoCollectMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CryptoCollectMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
